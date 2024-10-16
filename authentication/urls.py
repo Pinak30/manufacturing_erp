eb2a755c64b4dfaa0a2339e15d6d58b413a601f3
bhatnagarD <143522029+bhatnagarD@users.noreply.github.com>
2024-10-17 00:14:35 +0530

    HR Dashboard Created
    Created HR Module Dashboard and Employee Detail form

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import login
+from .views import *
+# from .views import login_view, register_user
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', login, name='login'),
+    # path('', login, name='login'),
+    path('', index, name="index"),
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('employee/', employee_detail, name="employee_detail"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,97 @@
-from django.shortcuts import render
+from django import template
+# from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, SignUpForm
 
-# Create your views here.
 
-def login(request):
-    return render(request, 'login.html')
+# def login(request):
+#     return render(request, 'login.html')
+
+# @login_required(login_url="/login/")
+def index(request):
+    context = {'segment': 'index'}
+
+    html_template = loader.get_template('home/index.html')
+    return HttpResponse(html_template.render(context, request))
+
+def employee_detail(request):
+    # context = {'segment': 'index'}
+    return render(request, 'home/profile.html')
+    # html_template = loader.get_template('home/profile.html')
+    # return HttpResponse(html_template.render(context, request))
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
+
+def register_user(request):
+    msg = None
+    success = False
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            msg = 'User created - please <a href="accounts/login">login</a>.'
+            success = True
+
+            # return redirect("/login/")
+
+        else:
+            msg = 'Form is not valid'
+    else:
+        form = SignUpForm()
+
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+
+# @login_required(login_url="/login/")
+# def pages(request):
+#     context = {}
+#     # All resource paths end in .html.
+#     # Pick out the html file name from the url. And load that template.
+#     try:
+
+#         load_template = request.path.split('/')[-1]
+
+#         if load_template == 'admin':
+#             return HttpResponseRedirect(reverse('admin:index'))
+#         context['segment'] = load_template
+
+#         html_template = loader.get_template('home/' + load_template)
+#         return HttpResponse(html_template.render(context, request))
+
+#     except template.TemplateDoesNotExist:
+
+#         html_template = loader.get_template('home/page-404.html')
+#         return HttpResponse(html_template.render(context, request))
+
+#     except:
+#         html_template = loader.get_template('home/page-500.html')
+#         return HttpResponse(html_template.render(context, request))
