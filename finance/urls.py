3b25b8c9974eccce4f367f35acefa19025182253
Pinak30 <pinakpimputkar123@gmail.com>
2024-10-31 16:29:04 +0530

    Finance module menu

--- finance/urls.py
+++ finance/urls.py
@@ -3,4 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('finance/', finance, name='finance'),
+    path('general_ledger/', generalLedger, name='generalLedger'),
+    path('transaction/', transaction, name='transaction'),
+    path('account/', account, name='account'),
+    path('invoice/', invoice, name='invoice'),
+    path('payment/', payment, name='payment'),
+    path('customer/', customer, name='customer'),
+    path('purchase_order/', purchaseorder, name='purchaseorder'),
+    path('expense/', expense, name='expense'),
+    path('budget/', budget, name='budget'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -7,3 +8,70 @@ def finance(request):
         'app_name': 'Finance',  # Pass app name dynamically
     }
         return render(request, 'home/finance.html',context)
+
+
+def generalLedger(request):
+        ledger = GeneralLedger.objects.all()
+        context = {
+        'app_name': 'Finance',  
+        'ledger': ledger,
+    }
+        return render(request, 'home/generalLedger.html',context)
+
+
+def transaction(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/transaction.html',context)
+
+
+def account(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/account.html',context)
+
+
+def invoice(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/invoice.html',context)
+
+
+def payment(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/payment.html',context)
+
+
+def customer(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/customer.html',context)
+
+
+def purchaseorder(request):
+        orders = PurchaseOrderFinance.objects.all()
+        context = {
+        'app_name': 'Finance',  
+        'orders': orders,
+    }
+        return render(request, 'home/purchaseorder.html',context)
+
+
+def expense(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/expense.html',context)
+
+
+def budget(request):
+        context = {
+        'app_name': 'Finance',  # Pass app name dynamically
+    }
+        return render(request, 'home/budget.html',context)
